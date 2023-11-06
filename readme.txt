4fb8fbc6dcb9c3710d57433a2ae2e56767d8e36c
davissand <davidsand314@gmail.com>
2023-11-06 09:26:16 -0600

    adding readme

---
+++ readme.txt
@@ -0,0 +1,3 @@
+py -m pip install Flask
+py -m pip install flask_cors
+py -m pip install flask_pymongo
--- src/app.py
+++ src/app.py
@@ -15,27 +15,6 @@ CORS(app)
 mongo = PyMongo(app)
 
 
-@app.route('/users', methods=['POST'])
-def create_user():
-    # Receiving Data
-    username = request.json['username']
-    email = request.json['email']
-    password = request.json['password']
-
-    if username and email and password:
-        hashed_password = generate_password_hash(password)
-        id = mongo.db.users.insert(
-            {'username': username, 'email': email, 'password': hashed_password})
-        response = jsonify({
-            '_id': str(id),
-            'username': username,
-            'password': password,
-            'email': email
-        })
-        response.status_code = 201
-        return response
-    else:
-        return not_found()
 
 
 @app.route('/clientes-autos', methods=['GET'])
@@ -146,37 +125,6 @@ def get_vehiculos_mayores(fecha):
     vehiculos =  list(mongo.db.Vehiculo.find({'anio': {'$gt': fecha}}))
     response = json_util.dumps(vehiculos)
     return Response(response, mimetype="application/json")
-
-@app.route('/users/<id>', methods=['GET'])
-def get_user(id):
-    print(id)
-    user = mongo.db.users.find_one({'_id': ObjectId(id), })
-    response = json_util.dumps(user)
-    return Response(response, mimetype="application/json")
-
-
-@app.route('/users/<id>', methods=['DELETE'])
-def delete_user(id):
-    mongo.db.users.delete_one({'_id': ObjectId(id)})
-    response = jsonify({'message': 'User' + id + ' Deleted Successfully'})
-    response.status_code = 200
-    return response
-
-
-@app.route('/users/<_id>', methods=['PUT'])
-def update_user(_id):
-    username = request.json['username']
-    email = request.json['email']
-    password = request.json['password']
-    if username and email and password and _id:
-        hashed_password = generate_password_hash(password)
-        mongo.db.users.update_one(
-            {'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {'username': username, 'email': email, 'password': hashed_password}})
-        response = jsonify({'message': 'User' + _id + 'Updated Successfuly'})
-        response.status_code = 200
-        return response
-    else:
-      return not_found()
 
 
 @app.errorhandler(404)
